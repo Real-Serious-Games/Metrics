aebd0b572c45e5aad363ff4559f16a669db747ec
Rory Dungan <rory.dungan@realseriousgames.com>
2015-10-20 15:03:03 +1000

    Added xUnit test runner

--- Tests/MetricsTests.cs
+++ Tests/MetricsTests.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace RSG.MetricsTests
 {
     public class MetricsTests
     {
         Metrics testObject;
+
+        [Fact]
+        public void entry_emits_message()
+        {
+            Assert.Equal(true, true);
+        }
     }
 }
