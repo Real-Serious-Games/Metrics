3a4da8b8a442b0fe6fee83384ed4d3327cb2089b
Philip Warren <philip.warren@realseriousgames.com>
2015-10-28 11:00:12 +1000

    Added example 3 with batching.

---
+++ Examples/Example3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RSG;
+
+namespace Example3
+{
+    /// <summary>
+    /// An example using the metrics system to send metrics via JSON to an HTTP service.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpService = new SimpleHttpService();
+            var postUrl = "http://localhost:3000/emit";
+
+            // Create the metrics with batching.
+            var metrics = new Metrics(new HttpJsonPostEmitter(postUrl, httpService), 5);
+
+            // Create some dummy metrics data
+            metrics.SetProperty("Metrics entry", "test");
+            for (var i = 0; i < 10; i++)
+            {
+                metrics.Entry("Test int entry", i);
+            }
+            metrics.SetProperty("Second property", "true");
+            metrics.Event("An event occurred");
+            metrics.Entry("Test string entry", "foo");
+
+            // Flush the metrics before in case there are still queued metrics.
+            metrics.Flush();
+
+            Console.WriteLine("Sent metrics data to " + postUrl);
+        }
+    }
+}
---
+++ Examples/Example3/SimpleHttpService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Example3
+{
+    /// <summary>
+    /// Simple implementation of IHttpService supporting posting data to a URL.
+    /// </summary>
+    class SimpleHttpService : IHttpService
+    {
+
+        public void Post(string url, string text)
+        {
+            // Convert to byte array
+            var data = Encoding.Default.GetBytes(text);
+
+            // Create request
+            var request = WebRequest.Create(url);
+
+            request.Method = "POST";
+            request.ContentLength = data.Length;
+            request.ContentType = "application/json";
+
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(data, 0, data.Length);
+            dataStream.Close();
+
+            // Send the request
+            var response = (HttpWebResponse)request.GetResponse();
+            response.Close();
+
+            // Notify if we were unsucessful
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new ApplicationException("POST to " + url + " returned response code " + 
+                    (int)response.StatusCode + " " + response.StatusCode.ToString());
+            }
+        }
+    }
+}
