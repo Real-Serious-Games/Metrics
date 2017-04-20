1ad79abbc71e2157bee83310a0e96669c88a4e5e
Rory Dungan <rorydungan@gmail.com>
2017-04-20 16:57:32 +1000

    Changed AssemblyTitle to RSG.Metrics
    Nuget wouldn't accept just "Metrics" any more because it is too similar to another library's ID.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Metrics")]
+[assembly: AssemblyTitle("RSG.Metrics")]
 [assembly: AssemblyDescription("A metrics output system for collecting data for analytics. ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Real Serious Games")]
